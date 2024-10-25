52db66214fea73d2ac7edecd1c91b694ee88d4b8
Mohammed Saabiq Saha <138797077+saabiqsaha@users.noreply.github.com>
2024-10-25 01:08:34 +0000

    progress

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-def minmax(data):
-    min_val = data[0]
-    max_val = data[0]
+def sum_of_square(n):
+    total = 0
+    for i in range(1, n):
+        total += i ** 2
+    return total
 
-    for num in data:
-        if num < min_val:
-            min_val = num
-        if num > max_val:
-            max_val = num
-    return(min_val, max_val)
-        
-data = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
-print(minmax(data))
+test = sum_of_square(5)
+
+print(test)

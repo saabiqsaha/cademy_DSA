f306b2079df78579ae3db8e106f765f0950b67b6
Mohammed Saabiq Saha <138797077+saabiqsaha@users.noreply.github.com>
2024-10-25 01:32:07 +0000

    mmm

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-def odd_less_sums(n):
-    total = 0
-    for i in range(1, n):
-        if i % 2 != 0:
-            total += i ** 2
-    return total
-
-
-test = odd_less_sums(6)
-print(test)
+for i in range(9):
+    print(2**i)

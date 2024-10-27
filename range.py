fc2b2d4371c22bc8b2f217bfc08526664a20ef45
Mohammed Saabiq Saha <138797077+saabiqsaha@users.noreply.github.com>
2024-10-27 21:46:55 +0000

    mmm

---
+++ range.py
@@ -0,0 +1,23 @@
+class Range:
+    def __init__(self, start, stop = None, step=1):
+        if step == 0:
+            raise ValueError('step cannot be zero')
+        if stop is None:
+            start = 0
+            stop = start
+
+        self._length = max(0, (stop - start + step -1) // step)
+        self._start = start
+        self._step = step
+
+    def __len__(self):
+        return self._length
+    
+    def __getitem__(self, k):
+        if k < 0:
+            k += len(self)
+
+        if not 0 <= k < self._length:
+            raise IndexError('index out of range')
+        
+        return self._start + k * self._step

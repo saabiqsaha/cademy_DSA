26ff0c60511fee6f833679044b896433b432af37
Mohammed Saabiq Saha <138797077+saabiqsaha@users.noreply.github.com>
2024-10-28 05:25:13 +0000

    mm

--- arithmetic_progression.py
+++ arithmetic_progression.py
@@ -17,7 +17,24 @@ class ArithmeticProgression:
     def get_difference(self) -> int:
         return self.difference
     def generator(self):
+        if self.difference == 0:
+            raise ValueError('difference cant be zero')
         sequence = []
         current = self.first
-        while current <= self.last
+        while current <= self.last:
+            sequence.append(current)
+            current += self.difference
+        return sequence
+    def sum_sequence(self):
+        if self.difference == 0:
+            raise ValueError('Difference cant be zero')
+        n = ((self.first + self.last) // self.difference) + 1
+        return (n * (self.first + self.last)) // 2
+    def __str__(self):
+        return f"Arithmetic Progression: first= {self.first}, last={self.last}, and difference={self.difference}"
     
+
+ap_test = ArithmeticProgression(first = 1, last = 12, difference = 5)
+sequence = ap_test.generator()
+print(ap_test)
+print(sequence)
